7e8e70776dac262a7a8665e5fb8798865cc33e09
abhinaps <abhinapsingh100@gmail.com>
2024-12-20 07:41:31 +0530

    Project Setup

---
+++ src/main/java/com/example/budgetbandit/BudgetBanditApplication.java
@@ -0,0 +1,13 @@
+package com.example.budgetbandit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBanditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetBanditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/budgetbandit/BudgetBanditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budgetbandit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBanditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
